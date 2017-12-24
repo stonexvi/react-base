f7395b96351c345df76fb21e16acab72dd02a2fe
Brendan Stone <stonexvi@gmail.com>
2017-12-24 02:12:37 -0500

    Updating Docker, Adding CSS

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = (props) => (<h1>Hello, World!</h1>)
+const App = (props) => (<h1 className="centerHeader"> eizst.com </h1>)
 
 ReactDOM.render(<App />,  document.getElementById('app'))
